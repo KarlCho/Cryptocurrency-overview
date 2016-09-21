3f2a1b2819c71e5f5aecc3bfb99a5bb5947d9c7c
Karl Cho <Nilex@windowslive.com>
2016-09-21 23:02:37 +1200

    heya

--- js/main.ts
+++ js/main.ts
@@ -6,12 +6,34 @@ function searchCrypto() {
     ticker = $("#ticker").val();
     if (ticker !== ""){
         $.get("https://coinmarketcap-nexuist.rhcloud.com/api/" + ticker, function(data){
-            var priceData = data["price"].usd;
-            var market_cap = data["market_cap"].usd;
-            var volume = data["volume"].usd;
-            document.getElementById("price").innerHTML = "$" + priceData;
-            document.getElementById("marketCap").innerHTML = "$" + market_cap;
-            document.getElementById("volumeTraded").innerHTML = "$" + volume;
+            var cryptoName = data["name"];
+            document.getElementById("cryptoName").innerHTML = cryptoName;
+            var supply = data["supply"];
+            document.getElementById("supply").innerHTML = "total supply " + supply;
+            var priceUS = data["price"].usd;
+            var marketUS = data["market_cap"].usd;
+            var volumeUS = data["volume"].usd;
+            document.getElementById("priceUSD").innerHTML = "$" + priceUS;
+            document.getElementById("marketUSD").innerHTML = "$" + marketUS;
+            document.getElementById("volumeUSD").innerHTML = "$" + volumeUS;
+            var priceEU = data["price"].eur;
+            var marketEU = data["market_cap"].eur;
+            var volumeEU = data["volume"].eur;
+            document.getElementById("priceEUR").innerHTML = "\u20AC" + priceEU;
+            document.getElementById("marketEUR").innerHTML = "\u20AC" + marketEU;
+            document.getElementById("volumeEUR").innerHTML = "\u20AC" + volumeEU;
+            var priceCN = data["price"].cny;
+            var marketCN = data["market_cap"].cny;
+            var volumeCN = data["volume"].cny;
+            document.getElementById("priceCNY").innerHTML = "\u00A5" + priceCN;
+            document.getElementById("marketCNY").innerHTML = "\u00A5" + marketCN;
+            document.getElementById("volumeCNY").innerHTML = "\u00A5" + volumeCN;
+            var priceAU = data["price"].aud;
+            var marketAU = data["market_cap"].aud;
+            var volumeAU = data["volume"].aud;
+            document.getElementById("priceAUD").innerHTML = "$" + priceAU;
+            document.getElementById("marketAUD").innerHTML = "$" + marketAU;
+            document.getElementById("volumeAUD").innerHTML = "$" + volumeAU;
 
         });
     }
